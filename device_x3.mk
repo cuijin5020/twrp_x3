34c724a453058192ccd14c54bc3fb4155ae8336e
Sai Vishal <saivishal2001@gmail.com>
2016-12-25 12:55:44 +0530

    ramdisk: initial files
    Signed-off-by: Sai Vishal <saivishal2001@gmail.com>

--- device_x3.mk
+++ device_x3.mk
@@ -33,4 +33,12 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.service.acm.enable=0 \
     persist.sys.usb.config=mtp
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.mt6795:root/fstab.mt6795 \
+    $(LOCAL_PATH)/rootdir/init.mt6595.rc:root/init.mt6595.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6795.rc:root/init.mt6795.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6595.rc:root/ueventd.rc
+    $(LOCAL_PATH)/rootdir/ueventd.mt6795.rc:root/ueventd.rc
+    $(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
