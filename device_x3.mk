76868f9b88bd0d59a263fe004c5472904004eeb4
Sai Vishal <saivishal2001@gmail.com>
2016-12-25 13:15:04 +0530

    ramdisk: fix product files
    How am I so careless?

--- device_x3.mk
+++ device_x3.mk
@@ -35,11 +35,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.mt6795:root/fstab.mt6795 \
-    $(LOCAL_PATH)/rootdir/init.mt6595.rc:root/init.mt6595.rc \
     $(LOCAL_PATH)/rootdir/init.mt6795.rc:root/init.mt6795.rc \
     $(LOCAL_PATH)/rootdir/init.rc:root/init.mt6795.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6595.rc:root/ueventd.rc
-    $(LOCAL_PATH)/rootdir/ueventd.mt6795.rc:root/ueventd.rc
+    $(LOCAL_PATH)/rootdir/ueventd.mt6595.rc:root/ueventd.mt6595.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6795.rc:root/ueventd.mt6795.rc \
     $(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=8
